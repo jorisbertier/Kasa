850eb009751d516cf9b3512022ddca2cb51113d5
Joris Bertier <joris.bertier@gmail.com>
2024-08-28 15:55:42 -0400

    add image & Route

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+        <div>
+            Nav bar
+        </div>
+    )
+}
+
+export default Navbar
--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/Home';
 import '../src/assets/style/main.scss'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
