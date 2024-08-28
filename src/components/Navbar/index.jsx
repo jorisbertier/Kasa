c46cb3013865d77bf323f76989995b24821c0608
Joris Bertier <joris.bertier@gmail.com>
2024-08-28 16:18:36 -0400

    add font montserrat

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,18 @@
+import Logo from '../../assets/images/LOGO.png'
+
 function Navbar() {
     return (
-        <div>
-            Nav bar
-        </div>
+        <nav>
+            <div>
+                <img src={Logo} alt="logo du site Kasa"/>
+            </div>
+            <div>
+                <ul>
+                    <li>Accueil</li>
+                    <li>A propos</li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
