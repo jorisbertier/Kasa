f6d0e3541fca02cb98471f0c132735d94404cf08
Joris Bertier <joris.bertier@gmail.com>
2024-08-30 07:54:22 -0400

    create Component Card

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,12 @@
+function Card() {
+    return (
+        <div>
+            <div className="home__gallery">
+                <img className="home__gallery--image"/>
+                <div className="home__gallery--title"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,27 +1,16 @@
 import Banner from "../../components/Banner"
 import image from '../../assets/images/ile.png'
+import Card from "../../components/Card"
 
 function Home() {
     return (
     <div>
         <Banner image={image} title="Chez vous, partout et ailleurs"/>
         <div className="home">
-            <div className="home__gallery">
-                <img className="home__gallery--image"/>
-                <div className="home__gallery--title">Titre de la location</div>
-            </div>
-            <div className="home__gallery">
-                <img className="home__gallery--image"/>
-                <div className="home__gallery--title"></div>
-            </div>
-            <div className="home__gallery">
-                <img className="home__gallery--image"/>
-                <div className="home__gallery--title"></div>
-            </div>
-            <div className="home__gallery">
-                <img className="home__gallery--image"/>
-                <div className="home__gallery--title"></div>
-            </div>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
         </div>
     </div>
 )
