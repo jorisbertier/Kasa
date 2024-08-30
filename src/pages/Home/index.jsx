f8c4674055d5bc6a0293792c075c1bcacd5f48b5
Joris Bertier <joris.bertier@gmail.com>
2024-08-30 08:16:20 -0400

    get data to home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,29 @@
 import Banner from "../../components/Banner"
 import image from '../../assets/images/ile.png'
 import Card from "../../components/Card"
+import data from '../../datas/Data.json'
 
 function Home() {
+
+    console.log(data)
     return (
     <div>
         <Banner image={image} title="Chez vous, partout et ailleurs"/>
         <div className="home">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {data.map((data) => (
+                <div key={`${data.id}`}>
+                    <Card title ={data.title}
+                        src={data.cover}
+                        description={data.description}
+                        id={data.id}
+                        pictures={data.pictures}
+                        rating={data.rating}
+                        equipments={data.equipments}
+                        location={data.location}
+                        tag={data.tags}
+                    />
+                </div>
+            ))}
         </div>
     </div>
 )
