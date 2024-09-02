9e4c83ed2f88b71cec2a7b71e98d05acf5534903
Joris Bertier <joris.bertier@gmail.com>
2024-09-02 15:39:40 -0400

    add useLocation for style navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,21 +1,26 @@
+import { Link } from 'react-router-dom'
 import Logo from '../../assets/images/LOGO.png'
+import { useLocation } from 'react-router-dom'
 
 function Navbar() {
+
+    const location = useLocation()
+
     return (
         <nav>
             <div className="nav__wrapper">
-                <a>
+                <Link to='/'> : 
                     <img src={Logo} alt="logo du site Kasa"/>
-                </a>
+                </Link>
             </div>
             <div className='nav__wrapper'>
                 <ul>
-                    <a href="#">
+                    <Link to="/" className={location.pathname === "/" ? 'active' : ''}>
                         <li>Accueil</li>
-                    </a>
-                    <a href="#">
+                    </Link>
+                    <Link to="/about" className={location.pathname === "/about" ? 'active' : ''}>
                         <li>A propos</li>
-                    </a>
+                    </Link>
                 </ul>
             </div>
         </nav>
